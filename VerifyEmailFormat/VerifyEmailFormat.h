99f46b7dc1bd1e408d75ac6fd8c0de5892a25485
dev lovewanshi <lovewanshi.devendra@gmail.com>
2023-10-17 16:17:36 +0530

    - Add Framework

---
+++ VerifyEmailFormat/VerifyEmailFormat.h
@@ -0,0 +1,18 @@
+//
+//  VerifyEmailFormat.h
+//  VerifyEmailFormat
+//
+//  Created by dlovewanshi on 17/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VerifyEmailFormat.
+FOUNDATION_EXPORT double VerifyEmailFormatVersionNumber;
+
+//! Project version string for VerifyEmailFormat.
+FOUNDATION_EXPORT const unsigned char VerifyEmailFormatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VerifyEmailFormat/PublicHeader.h>
+
+
