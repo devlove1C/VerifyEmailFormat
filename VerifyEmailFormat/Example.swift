be81bd16782358360fbbb6e444a8d77e28a367bf
dev lovewanshi <lovewanshi.devendra@gmail.com>
2023-10-17 17:03:55 +0530

    - Add Example file to test SDK methods

---
+++ VerifyEmailFormat/Example.swift
@@ -0,0 +1,15 @@
+//
+//  Example.swift
+//  TataSDK
+//
+//  Created by dlovewanshi on 12/10/23.
+//
+
+import Foundation
+public class Example {
+    public init(){}
+    
+    public func printMesaage(messageString : String) {
+        print("Hello handsome, how are you? your passed message is: -\(messageString)")
+    }
+}
